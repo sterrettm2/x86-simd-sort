66bdd60a312e504543a3deffb95154ae574b9585
Matthew Sterrett <matthew.sterrett@intel.com>
2023-09-11 12:52:23 -0700

    Enabled comparative benchmarks with VQSort

--- benchmarks/bench-qsort.hpp
+++ benchmarks/bench-qsort.hpp
@@ -1,3 +1,5 @@
+#define VQSORT_ONLY_STATIC 1
+#include "hwy/contrib/sort/vqsort-inl.h"
 #include "bench-qsort-common.h"
 
 template <typename T, class... Args>
@@ -80,15 +82,58 @@ static void avx512qsort(benchmark::State &state, Args &&...args)
     }
 }
 
+template <typename T, class... Args>
+static void vqsort(benchmark::State &state, Args &&...args)
+{
+    auto args_tuple = std::make_tuple(std::move(args)...);
+    // Perform setup here
+    size_t ARRSIZE = std::get<0>(args_tuple);
+    std::vector<T> arr;
+    std::vector<T> arr_bkp;
+
+    std::string arrtype = std::get<1>(args_tuple);
+    if (arrtype == "random") { arr = get_uniform_rand_array<T>(ARRSIZE); }
+    else if (arrtype == "sorted") {
+        arr = get_uniform_rand_array<T>(ARRSIZE);
+        std::sort(arr.begin(), arr.end());
+    }
+    else if (arrtype == "constant") {
+        T temp = get_uniform_rand_array<T>(1)[0];
+        for (size_t ii = 0; ii < ARRSIZE; ++ii) {
+            arr.push_back(temp);
+        }
+    }
+    else if (arrtype == "reverse") {
+        arr = get_uniform_rand_array<T>(ARRSIZE);
+        std::sort(arr.begin(), arr.end());
+        std::reverse(arr.begin(), arr.end());
+    }
+    arr_bkp = arr;
+
+    /* call avx512 quicksort */
+    for (auto _ : state) {
+        hwy::HWY_NAMESPACE::VQSortStatic(arr.data(), ARRSIZE, hwy::SortAscending());
+        state.PauseTiming();
+        arr = arr_bkp;
+        state.ResumeTiming();
+    }
+}
+
+#define BENCH_WITH_VQSORT(type) \
+    BENCH(avx512qsort, type) \
+    BENCH(stdsort, type) \
+    BENCH(vqsort, type)
+    
 #define BENCH_BOTH_QSORT(type) \
     BENCH(avx512qsort, type) \
     BENCH(stdsort, type)
 
-BENCH_BOTH_QSORT(uint64_t)
-BENCH_BOTH_QSORT(int64_t)
-BENCH_BOTH_QSORT(uint32_t)
-BENCH_BOTH_QSORT(int32_t)
+BENCH_WITH_VQSORT(uint64_t)
+BENCH_WITH_VQSORT(int64_t)
+BENCH_WITH_VQSORT(uint32_t)
+BENCH_WITH_VQSORT(int32_t)
+BENCH_WITH_VQSORT(float)
+BENCH_WITH_VQSORT(double)
+
 BENCH_BOTH_QSORT(uint16_t)
 BENCH_BOTH_QSORT(int16_t)
-BENCH_BOTH_QSORT(float)
-BENCH_BOTH_QSORT(double)
